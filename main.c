1d495fc2c83a2a496770550f5317ecc760d427e0
ChristopherPhillippe <Christopher-Philippe.JAUREGUI@cpnv.ch>
2022-02-02 16:01:50 +0100

    Chandeleur
    Ce programme calcule les ingrédients d'une recette pour les crêpes

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-int ingredients(int personnum) {
-    int farine, sel, beurre;
+int farine(int personnum){
+    int farine;
     farine = personnum * 125;
+    return farine;
+}
+
+float lait(int personnum){
     float lait = personnum * 0.25;
+    return lait;
+}
+
+float oeufs(int personnum){
     float oeufs = personnum * 1.5;
+    return oeufs;
+}
+
+float sel(int personnum){
+    float sel = personnum / 2;
+    return sel;
+}
+
+float beurre(int personnum) {
+    float beurre = personnum / 2;
+    return beurre;
 }
 
 
@@ -15,6 +34,11 @@ int main() {
     printf("Entrez le nombre de personnes qui mangeront des crepes: ");
     scanf("%d", &personnum);
     printf("\n\nLes ingredients necessaires pour 6 personnes sont: ");
+    printf("\nFarine: %d g", farine(personnum));
+    printf("\nLait: %f l", lait(personnum));
+    printf("\nOeufs: %f", oeufs(personnum));
+    printf("\nSel: %f pincees", sel(personnum));
+    printf("\nBeurre: %f cuilleres", beurre(personnum));
 
     return 0;
 }
